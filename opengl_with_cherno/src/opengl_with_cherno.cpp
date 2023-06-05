1eccb5b27576d2f00b6c141b20afeeafbb2c57f2
LStraight <1397983291@qq.com>
2023-06-05 15:02:12 +0800

    video_7

--- opengl_with_cherno/src/opengl_with_cherno.cpp
+++ opengl_with_cherno/src/opengl_with_cherno.cpp
@@ -3,6 +3,48 @@
 
 #include <iostream>
 
+static unsigned int ComplieShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    
+    //TODO: Error handing 
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = new char[length];
+        glGetShaderInfoLog(id, length, &length, message);
+
+        std::cout << "Failed to compile shader! " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
+        std::cout << message << std::endl;
+
+        glDeleteShader(id);
+        delete[] message;
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertShader, const std::string& fragShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = ComplieShader(GL_VERTEX_SHADER, vertShader);
+    unsigned int fs = ComplieShader(GL_FRAGMENT_SHADER, fragShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -37,7 +79,7 @@ int main(void)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
     /*
-    * 
+    * 层数，layout
     * 顶点属性数量：此处只有二维坐标，所以当传入2，如果除了坐标还有别的属性，比如纹理法向，则按实际填入
     * 数据类型：此处为float
     * 需不需要正则化：此处已正则化，填false
@@ -45,7 +87,22 @@ int main(void)
     * 
     */
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    std::string vertexShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main(){\n"
+        "   gl_Position = position;\n"
+        "}\n";
+    std::string fragmentShader = 
+        "#version 330 core\n"
+        "out vec4 color;\n"
+        "void main(){\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -57,6 +114,8 @@ int main(void)
 
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
