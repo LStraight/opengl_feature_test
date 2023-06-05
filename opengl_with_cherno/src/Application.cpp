8aab7ac34eed8536bdbd9cd1f5a1e1e85dee2a7c
LStraight <1397983291@qq.com>
2023-06-05 15:48:26 +0800

    combine shader file into on file and how to pick them out

--- opengl_with_cherno/src/Application.cpp
+++ opengl_with_cherno/src/Application.cpp
@@ -2,6 +2,42 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+struct ShaderSources {
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderSources ParseShader(const std::string& filepath) {
+    std::ifstream stream(filepath);
+
+    enum class ShaderType {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+
+    while (getline(stream, line)) {
+        if (line.find("#shader") != std::string::npos) {
+            if (line.find("vertex") != std::string::npos) {
+                type = ShaderType::VERTEX;
+            }
+            else if (line.find("fragment") != std::string::npos) {
+                type = ShaderType::FRAGMENT;
+            }
+        }
+        else {
+            ss[int(type)] << line << '\n';
+        }
+    }
+
+    return { ss[0].str(), ss[1].str() };
+}
 
 static unsigned int ComplieShader(unsigned int type, const std::string& source) {
     unsigned int id = glCreateShader(type);
@@ -87,23 +123,10 @@ int main(void)
     * 
     */
 
-    std::string vertexShader = 
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) in vec4 position;\n"
-        "\n"
-        "void main(){\n"
-        "   gl_Position = position;\n"
-        "}\n";
-    std::string fragmentShader = 
-        "#version 330 core\n"
-        "out vec4 color;\n"
-        "void main(){\n"
-        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-        "}\n";
-    unsigned int shader = CreateShader(vertexShader, fragmentShader);
-    glUseProgram(shader);
-
+    
+    ShaderSources source = ParseShader("res/shaders/Basic.shader");
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+     glUseProgram(shader);
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
